195d469cecb7c56914339c322c9c7307a17ddd06
jgargar <jacksongargar34@gmail.com>
2023-10-21 05:46:06 -0700

    this is my new commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * hare_main - funcion that prints a specific string before the main,
+ * function in executed.
+ *
+ * Return: no return.
+ */
+
+
+void hare_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
